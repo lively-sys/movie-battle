689d0393d38f322bbc97bd6ff6e13f3d5f293c56
rlively03 <rlively@calpoly.edu>
2026-08-09 11:34:31 -0700

    Created a method of getting the number of Oscar Nominations and Wins for a given movie

---
+++ OscarBase.py
@@ -0,0 +1,35 @@
+#List of functions used to get data from the OscarBase API
+
+#Gets the number of unique nominations for a movie given its TMDB ID
+def get_nominations(tmdb_id):
+    import requests
+
+    solo_awards=["Directing","Actor In A Leading Role","Actor In A Supporting Role","Actress In A Leading Role","Actress In A Supporting Role"]
+
+    url_a = "https://api.oscarbase.com/api/movies"
+    params = {
+        "tmdb_id": tmdb_id
+    }
+    response = requests.get(url_a, params=params)
+
+    url_b=f"https://api.oscarbase.com/api/movies/{response.json()['data'][0]['id']}"
+
+    nominations = requests.get(url_b).json()["data"]["nominations"]
+    unique_nominations = []
+    seen_categories = set()
+
+    for nomination in nominations:
+        if nomination["category"] in solo_awards or nomination["category"] not in seen_categories:
+            unique_nominations.append(nomination)
+            seen_categories.add(nomination["category"])
+    unique_winners = [
+        nomination
+        for nomination in unique_nominations
+        if nomination["winner"]
+    ]
+
+    output = {
+        "noms": len(unique_nominations),
+        "wins": len(unique_winners)
+    }
+    return output
